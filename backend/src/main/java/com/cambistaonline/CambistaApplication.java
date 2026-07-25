a92732c32a9b99663bc248e16096bf048c726ed6
pingponista <alexaparicio.web@gmail.com>
2026-07-25 02:49:33 -0500

    cambio de claves, variables de entorno agregados

--- backend/src/main/java/com/cambistaonline/CambistaApplication.java
+++ backend/src/main/java/com/cambistaonline/CambistaApplication.java
@@ -1,5 +1,6 @@
 package com.cambistaonline;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CambistaApplication {
 
     public static void main(String[] args) {
+        try {
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            dotenv.entries().forEach(entry -> {
+                if (System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey()) == null) {
+                    System.setProperty(entry.getKey(), entry.getValue());
+                }
+            });
+        } catch (Exception ignored) {}
+
         SpringApplication.run(CambistaApplication.class, args);
     }
 }
